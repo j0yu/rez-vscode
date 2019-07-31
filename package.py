4482103e72c44c0f37eda8b766a63502fbf50664
Joseph Yu <josephyu2712+github@gmail.com>
2019-07-31 09:36:57 +0100

    Initial commit for version 1.36.1

---
+++ package.py
@@ -0,0 +1,29 @@
+name = 'vscode_stable'
+
+__version__ = '1.36.1'
+version = __version__ + '.local.1.0.0'
+
+__url__ = 'https://update.code.visualstudio.com/{version}/linux-x64/stable'
+__url__ = __url__.format(version=__version__)
+
+build_command = '''
+set -euf -o pipefail
+
+if [ $REZ_BUILD_INSTALL -eq 1 ]
+then
+    cd $REZ_BUILD_INSTALL_PATH
+    curl -L -s {url} | tar --strip-components=1 -xz
+fi
+'''.format(url=__url__)
+
+def commands():
+    import os.path
+    env.PATH.append(os.path.join('{root}', 'bin'))
+
+
+@late()
+def tools():
+    import os
+    bin_path = os.path.join(str(this.root), 'bin')
+    return os.listdir(bin_path)
+
